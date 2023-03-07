3432d1b577be14a4c860889eaf26238071a618af
sneha <snehnang@udel.edu>
2023-03-07 14:54:21 -0500

    Task 7 - getNames function

--- src/nested.ts
+++ src/nested.ts
@@ -83,7 +83,14 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    const deepCopy = questions.map(
+        (question: Question): Question => ({
+            ...question,
+            options: [...question.options]
+        })
+    );
+    const names = deepCopy.map((ques: Question): string => ques.name);
+    return names;
 }
 
 /***
